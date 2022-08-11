8cba2a07404d77784873b7f87503ddb8a299f4ee
Anirudh Sharma <anirudh03sharma@gmail.com>
2022-08-11 18:10:48 +0530

    Initial Commit

---
+++ src/test/java/org/redquark/web3/DopeChain/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.redquark.web3.DopeChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
